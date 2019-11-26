b8fac20f1bca256b0b88a9856c43205a40841469
xhiroki.nakamura <xhiroki.nakamura@O-07919-Mac.local>
2019-11-26 18:00:26 +0900

    gulpや構成変更

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,27 @@ const gulp = require("gulp");
 // Sassをコンパイルするプラグインの読み込み
 const sass = require("gulp-sass");
 
-// style.scssをタスクを作成する
+// style.scssの監視タスクを作成する
 gulp.task("default", function () {
-    // style.scssファイルを取得
-    return (
-        gulp
-            .src("resources/assets/sass/*.scss")
-            // Sassのコンパイルを実行
-            .pipe(sass({outputStyle: "expanded"}))
-            // cssフォルダー以下に保存
-            .pipe(gulp.dest("resources/src/css"))
-    );
+    // ★ style.scssファイルを監視
+    return gulp.watch("resources/assets/sass/*.scss", function () {
+        // style.scssの更新があった場合の処理
+
+        // style.scssファイルを取得
+        return (
+            gulp
+                .src("resources/assets/sass/*.scss")
+                // Sassのコンパイルを実行
+                .pipe(
+                    sass({
+                        outputStyle: "expanded"
+                    })
+                        // Sassのコンパイルエラーを表示
+                        // (これがないと自動的に止まってしまう)
+                        .on("error", sass.logError)
+                )
+                // cssフォルダー以下に保存
+                .pipe(gulp.dest("resources/src/css"))
+        );
+    });
 });
